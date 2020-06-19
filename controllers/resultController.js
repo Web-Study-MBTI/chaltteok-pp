a1e7bdaa56fe9741b7fbfe88d65cd958c629edfe
DaEunShim <10ekdms01@naver.com>
2020-06-20 03:49:46 +0900

    complete [Front-Ed] Detail View

---
+++ controllers/resultController.js
@@ -0,0 +1,27 @@
+let statusCode = require('../modules/statusCode');
+let util = require('../modules/util');
+let Type = require('../models/type');
+
+exports.readResult = async (req,res)=>{
+    const type_idx = req.params.type_idx;
+
+    try{
+        const type = await Type.readResult(type_idx);
+        return res.status(statusCode.OK).send(res.render("result",{data: type[0]}));
+    } catch(err){
+        return res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, err.message));
+        throw err;
+    }
+};
+
+exports.readDetail = async (req,res)=>{
+    const type_idx = req.params.type_idx;
+
+    try{
+        const type = await Type.readResult(type_idx);
+        return res.status(statusCode.OK).send(res.render("detail",{data: type[0]}));
+    } catch(err){
+        return res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, err.message));
+        throw err;
+    }
+};
