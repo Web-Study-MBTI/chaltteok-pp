def7676c99a875df997057948030eb4f7947edc0
DaEunShim <10ekdms01@naver.com>
2020-07-25 15:05:17 +0900

    fix url

--- public/js/index1.js
+++ public/js/index1.js
@@ -206,7 +206,7 @@ $(".trigger4").click(function () {
    })
    // HTTP 요청이 성공하면 요청한 데이터가 done() 메소드로 전달됨.
    .done(function(json) {
-     location.href= `http://52.79.218.88:3000/result/${json.idx}`
+     location.href= `http://chaltteok-pp.cf/result/${json.idx}`
    })
    // HTTP 요청이 실패하면 오류와 상태에 관한 정보가 fail() 메소드로 전달됨.
    .fail(function(xhr, status, errorThrown) {
