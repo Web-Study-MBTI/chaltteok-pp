21e57ec28acca8214c0ee4bf8e7ce388b77e9c91
hyowon <dndwls9252@naver.com>
2020-07-31 16:04:51 +0900

    [UPDATE] user info

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,14 +18,14 @@ signup : async (req, res) => {
         return;
     }
     //already ID
-    const idx1 = await User.checkUser(user_name);
-    console.log(idx1+" : " + idx1[0])
-    if (idx1>0) {
-        // res.status(statusCode.BAD_REQUEST)
-        //     .send(util.fail(statusCode.BAD_REQUEST, resMessage.ALREADY_ID));
-        res.send('<script type="text/javascript">alert("이미 존재하는 닉네임입니다.");location.href="./user";</script>');
-        return;
-    }
+    // const idx1 = await User.checkUser(user_name);
+    // console.log(idx1+" : " + idx1[0])
+    // if (idx1>0) {
+    //     // res.status(statusCode.BAD_REQUEST)
+    //     //     .send(util.fail(statusCode.BAD_REQUEST, resMessage.ALREADY_ID));
+    //     res.send('<script type="text/javascript">alert("이미 존재하는 닉네임입니다.");location.href="./user";</script>');
+    //     return;
+    // }
     const idx = await User.signup(user_name,user_part);
     if (idx === -1) {
         return res.status(statusCode.DB_ERROR)
